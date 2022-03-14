1c15ca9b72f7454d5a21de45904e5faf73092b14
hefabao <hefabao@126.com>
2022-03-14 08:54:29 +0800

    第一步,使用赣州...web3查看帐号信息,web3交易eth

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,5 +1,9 @@
-const Web3 = require('web3')
-const rpcURL = '' // Your RCkP URL goes here
-const web3 = new Web3(rpcURL)
-const address = '' // Your account address goes here
-web3.eth.getBalance(address, (err, wei) => { balance = web3.utils.fromWei(wei, 'ether') })
+const Web3 = require("web3");
+const { rpcURL, address } = require("./env"); // Your RCkP URL goes here
+const web3 = new Web3(rpcURL);
+web3.eth.getBalance(address, (err, wei) => {
+  balance = web3.utils.fromWei(wei, "ether");
+  console.log(balance);
+  console.log(balance);
+  console.log(balance);
+});
